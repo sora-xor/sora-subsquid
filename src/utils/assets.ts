9fe3d86e4ef597ee3da7e9aca2bc1c234d3fd8a5
RDMStreet <RDMStreetOne@gmail.com>
2023-06-10 15:33:47 +0900

    Update asset snapshot open price

--- src/utils/assets.ts
+++ src/utils/assets.ts
@@ -170,6 +170,11 @@ class AssetSnapshotsStorage {
 				snapshot.priceUSD.close = price
 				snapshot.priceUSD.high = BigNumber.max(new BigNumber(snapshot.priceUSD.high), bnPrice).toString()
 				snapshot.priceUSD.low = BigNumber.min(new BigNumber(snapshot.priceUSD.low), bnPrice).toString()
+
+				// set open price to current price at first update (after start or restart)
+				if (Number(snapshot.priceUSD.open) === 0) {
+				  snapshot.priceUSD.open = price;
+				}
 			} else {
 				throw new Error(`${snapshot.id} snapshot doesn't have priceUSD`)
 			}
