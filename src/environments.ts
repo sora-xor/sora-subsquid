49e90fcfd58b8a4a85b11614ebe28d23e309c172
RDMStreet <RDMStreetOne@gmail.com>
2023-09-24 15:14:01 +0300

    Generate squid yaml script

--- src/environments.ts
+++ src/environments.ts
@@ -5,9 +5,22 @@ export enum Environment {
 	DEV = 'dev'
 }
 
+enum ScaleProfile {
+	SMALL = 'small',
+	MEDIUM = 'medium',
+	LARGE = 'large',
+}
+
 interface EnvironmentAddresses {
+	name: string
+	description: string
 	chain: string
 	archive: string
+	scaleProfiles: {
+		postgres: ScaleProfile
+		processor: ScaleProfile
+		api: ScaleProfile
+	}
 }
 
 type Environments = {
@@ -16,20 +29,48 @@ type Environments = {
 
 export const environments: Environments = {
 	[Environment.PRODUCTION]: {
+		name: 'sora',
+		description: 'Sora squid',
 		chain: 'wss://mof2.sora.org',
-		archive: 'https://sora.archive.subsquid.io/graphql'
+		archive: 'https://sora.archive.subsquid.io/graphql',
+		scaleProfiles: {
+			postgres: ScaleProfile.SMALL,
+			processor: ScaleProfile.LARGE,
+			api: ScaleProfile.SMALL,
+		}
 	},
 	[Environment.STAGE]: {
+		name: 'sora-stage',
+		description: 'Sora stage squid',
 		chain: 'wss://ws.framenode-7.s4.stg1.sora2.soramitsu.co.jp',
-		archive: 'https://sora-testnet.archive.subsquid.io/graphql'
+		archive: 'https://sora-testnet.archive.subsquid.io/graphql',
+		scaleProfiles: {
+			postgres: ScaleProfile.SMALL,
+			processor: ScaleProfile.SMALL,
+			api: ScaleProfile.SMALL,
+		}
 	},
 	[Environment.TEST]: {
+		name: 'sora-test',
+		description: 'Sora test squid',
 		chain: 'wss://ws.framenode-1.v1.tst.sora2.soramitsu.co.jp',
-		archive: 'https://subsquid.sq1.tst.sora2.soramitsu.co.jp/graphql'
+		archive: 'https://subsquid.sq1.tst.sora2.soramitsu.co.jp/graphql',
+		scaleProfiles: {
+			postgres: ScaleProfile.SMALL,
+			processor: ScaleProfile.SMALL,
+			api: ScaleProfile.SMALL,
+		}
 	},
 	[Environment.DEV]: {
+		name: 'sora-dev',
+		description: 'Sora dev squid',
 		chain: 'wss://ws.framenode-2.r0.dev.sora2.soramitsu.co.jp',
-		archive: 'https://subsquid.sq1.dev.sora2.soramitsu.co.jp/graphql'
+		archive: 'https://subsquid.sq1.dev.sora2.soramitsu.co.jp/graphql',
+		scaleProfiles: {
+			postgres: ScaleProfile.SMALL,
+			processor: ScaleProfile.SMALL,
+			api: ScaleProfile.SMALL,
+		}
 	},
 }
 
