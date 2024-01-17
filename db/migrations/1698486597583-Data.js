690be8f9fda5bba6d4885c02510863c7d0a1f55f
RDMStreet <RDMStreetOne@gmail.com>
2024-01-17 18:32:38 +0300

    Block height migration fix

--- db/migrations/1698486597583-Data.js
+++ db/migrations/1698486597583-Data.js
@@ -12,15 +12,12 @@ module.exports = class Data1698486597583 {
         await db.query(`ALTER TABLE "history_element" ALTER COLUMN "name" DROP DEFAULT`)
         await db.query(`ALTER TABLE "history_element" ALTER COLUMN "name" SET NOT NULL`)
         await db.query(`ALTER TABLE "staking_reward" ADD "payee_type" character varying(10) NOT NULL`)
-        await db.query(`ALTER TABLE "history_element" DROP COLUMN "block_height"`)
-        await db.query(`ALTER TABLE "history_element" ADD "block_height" integer NOT NULL`)
+        await db.query(`ALTER TABLE "history_element" ALTER COLUMN "block_height" SET NOT NULL`)
     }
 
     async down(db) {
         // Removing 'name' field
         await db.query(`ALTER TABLE "history_element" DROP COLUMN "name"`)
         await db.query(`ALTER TABLE "staking_reward" DROP COLUMN "payee_type"`)
-        await db.query(`ALTER TABLE "history_element" ADD "block_height" numeric NOT NULL`)
-        await db.query(`ALTER TABLE "history_element" DROP COLUMN "block_height"`)
     }
 }
