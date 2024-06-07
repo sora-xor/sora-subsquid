f4b54f31299bd8a507af40529ff4900a9085dc77
RDMStreet <RDMStreetOne@gmail.com>
2024-06-07 13:20:45 +0300

    Fix

--- src/utils/kensetsu.ts
+++ src/utils/kensetsu.ts
@@ -4,17 +4,17 @@ import { BlockContext } from "../types"
 import { getUtilsLog } from "./logs"
 
 export const getVaultAccountEntity = async (ctx: BlockContext, accountAddress: string) => {
-  let account = await ctx.store.get(VaultAccount, accountAddress)
+	let account = await ctx.store.get(VaultAccount, accountAddress)
 
-  if (account) {
+	if (account) {
 		getUtilsLog(ctx).debug({ address: accountAddress }, 'VaultAccount found in store')
 	} else {
-    account = new VaultAccount({
-		id: accountAddress
-	})
-    await ctx.store.save(account)
-    getUtilsLog(ctx).debug({ address: accountAddress }, 'VaultAccount created')
-  }
+		account = new VaultAccount({
+			id: accountAddress
+		})
+		await ctx.store.save(account)
+		getUtilsLog(ctx).debug({ address: accountAddress }, 'VaultAccount created')
+	}
 
-  return account
+	return account
 };
